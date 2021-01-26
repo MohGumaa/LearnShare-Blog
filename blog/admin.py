9c063675d6ed4510e9500d1ec4f8b6b133bab307
Mohamed Gumaa Abdalla Mohamed <gomaam14@gmail.com>
2021-01-26 21:27:14 +0400

    change site header and title

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+admin.site.site_header = 'LearnShare'
+admin.site.site_title = 'LearnShare Admin Area'
+admin.site.index_title = 'Welcome to admin area'
+
+# Add Model
+admin.site.register(Post)
