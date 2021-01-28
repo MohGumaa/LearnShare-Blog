f87bdbccce70cb70e8b335f124bfd978b03a1f53
Mohamed Gumaa Abdalla Mohamed <gomaam14@gmail.com>
2021-01-28 22:01:22 +0400

    create profile route

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method  == 'POST':
@@ -8,13 +9,21 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}')
-            return redirect('blog-home')
+            messages.success(request, f'Your account has been created! Your are now able to login')
+            return redirect('login')
     else:
         form = RegisterForm()
 
     context = {
-        'form': form
+        'form': form,
+        'title_page': 'Login'
     }
 
     return render(request, 'users/register.html', context)
+
+@login_required
+def profile(request):
+    context = {
+        'title_page': 'Profile'
+    }
+    return render(request, 'users/profile.html', context)
