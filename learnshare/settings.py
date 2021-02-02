40494f1019d8d63dfe04f1b9c1679eda52c198d5
Mohamed Gumaa Abdalla Mohamed <gomaam14@gmail.com>
2021-02-02 21:19:17 +0400

    add settings for send email

--- learnshare/settings.py
+++ learnshare/settings.py
@@ -121,3 +121,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('GOOGLE_USER')
+EMAIL_HOST_PASSWORD = config('GOOGLE_PASS')
